81a09d58a6e25db109c6704bfa72c4f1efbd03f1
d.shvetsov <inspairax230801@mail.ru>
2023-06-22 01:15:49 +0500

    feature-setting_up_the_entry_point: правит ошибку миграций

--- database/migrations/2023_06_20_113954_create_task_user_table.php
+++ database/migrations/2023_06_20_113954_create_task_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateTasksUserPivotTable extends Migration
+class CreateTaskUserTable extends Migration
 {
     /**
      * Run the migrations.
