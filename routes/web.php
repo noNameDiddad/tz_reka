f882151f2c23d8458b9602208ddca2edea5f5a48
d.shvetsov <inspairax230801@mail.ru>
2023-06-22 01:04:35 +0500

    feature-setting_up_the_entry_point: изменение главной страницы

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['verify' => true]);
 
 Route::middleware('verified')->group(function () {
 
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
 });
