e2267ab720293ea6c1a52780a120375c8a2a9803
d.shvetsov <inspairax230801@mail.ru>
2023-06-22 21:18:07 +0500

    feature-file_management: реализует методы управления  сущностью File

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,40 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return mixed
      */
     public function store(Request $request)
     {
-        //
+        $uploaded_file = $request->file('uploaded_file');
+        return File::create([
+            'path' => $uploaded_file->store('files/' . now()->format('d-m-Y'), 'public'),
+            'name' => $uploaded_file->getClientOriginalName(),
+            'preview_path' => '',
+            'filetype' => $uploaded_file->getClientMimeType(),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param File $file
+     * @return bool
      */
-    public function update(Request $request, File $file)
+    public function update(Request $request, File $file): bool
     {
-        //
+        $uploaded_file = $request->file('uploaded_file');
+        $path = $uploaded_file->store('files/' . now()->format('d-m-Y'), 'public');
+        if (Storage::exists('/public/' . $file->path)) {
+            Storage::delete('/public/' . $file->path);
+        }
+        return $file->update([
+            'path' => $path,
+            'name' => $uploaded_file->getClientOriginalName(),
+            'preview_path' => '',
+            'filetype' => $uploaded_file->getClientMimeType(),
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\File  $file
-     * @return \Illuminate\Http\Response
+     * @param File $file
+     * @return bool
      */
-    public function destroy(File $file)
+    public function destroy(File $file): bool
     {
-        //
+        if (Storage::exists('/public/' . $file->path)) {
+            Storage::delete('/public/' . $file->path);
+        }
+        return $file->delete();
     }
 }
